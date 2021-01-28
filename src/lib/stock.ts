3767e1853739fe07ba837dabe09381d7f3749972
Larry Legend <lparks@leadofftech.com>
2021-01-28 08:31:21 -0600

    added endpoint

--- src/lib/stock.ts
+++ src/lib/stock.ts
@@ -3,13 +3,16 @@ import financial from './financial';
 
 export default (stock) => {
   return {
-    profile: () => makeRequest('company/profile', generateJson(stock)),
-    quote: () => makeRequest('quote', generateJson(stock)),
     financial: financial(stock),
+    list: () => makeRequest('stock/list'),
+    quote: () => makeRequest('quote', generateJson(stock)),
     rating: () => makeRequest('company/rating', generateJson(stock)),
+    profile: () => makeRequest('company/profile', generateJson(stock)),
     current_price: () => makeRequest('stock/real-time-price', generateJson(stock)),
+    intraday: ({ interval }) => makeRequest(`historical-chart/${interval}`, generateJson(stock)),
+    news: ({ tickers, limit }) => makeRequest('stock_news', generateJson(null, { tickers, limit })),
     history: ({ start_date, end_date, data_type, limit }) => makeRequest('historical-price-full', generateJson(stock, { from: start_date, to: end_date, serietype: data_type, timeseries: limit })),
     dividend_history: ({ start_date, end_date, data_type, limit }) => makeRequest('historical-price-full/stock_dividend', generateJson(stock, { from: start_date, to: end_date, serietype: data_type, timeseries: limit })),
-    split_history: ({ start_date, end_date, data_type, limit }) => makeRequest('historical-price-full/stock_split', generateJson(stock, { from: start_date, to: end_date, serietype: data_type, timeseries: limit }))
+    split_history: ({ start_date, end_date, data_type, limit }) => makeRequest('historical-price-full/stock_split', generateJson(stock, { from: start_date, to: end_date, serietype: data_type, timeseries: limit })),
   }
 }
