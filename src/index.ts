300e87d10c0909b08d147dc6d46ceb73f72db96d
yryuu <yryuuyamazaki@gmail.com>
2021-06-06 10:50:20 +0900

    set up key method

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
 import commodities from "./lib/commodities";
-import crypto from './lib/crypto';
-import etf from './lib/etf';
-import forex from './lib/forex';
-import mutualfund from './lib/mutualfund';
-import stock from './lib/stock';
-import market from './lib/market';
-import search from './lib/search';
-import auth from './lib/auth';
-
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-auth.key = process.env.FMP_API_KEY!;
+import crypto from "./lib/crypto";
+import etf from "./lib/etf";
+import forex from "./lib/forex";
+import mutualfund from "./lib/mutualfund";
+import stock from "./lib/stock";
+import market from "./lib/market";
+import search from "./lib/search";
+import init from "./lib/init";
 
 export default {
+  init,
   stock,
   market,
-  forex, 
+  forex,
   crypto,
   search,
   commodities,
   etf,
-  mutualfund
-}
+  mutualfund,
+};
